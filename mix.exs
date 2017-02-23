d73c2ff6e7c97d293155d7cc60281f865e137f8b
Troy Nichols <troygnichols@gmail.com>
2017-02-23 13:12:46 -0500

    Update nerves dependencies

--- mix.exs
+++ mix.exs
@@ -8,11 +8,11 @@ defmodule NervesSystemNanopiNeo.Mixfile do
   def project do
     [app: :nerves_system_nanopi_neo,
      version: @version,
-     elixir: "~> 1.2",
-     compilers: Mix.compilers ++ [:nerves_system],
-     description: description,
-     package: package,
-     deps: deps]
+     elixir: "~> 1.3",
+     compilers: Mix.compilers ++ [:nerves_package],
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
   def application do
@@ -20,9 +20,9 @@ defmodule NervesSystemNanopiNeo.Mixfile do
   end
 
   defp deps do
-    [{:nerves_system, "~> 0.1.4"},
-     {:nerves_system_br, "~> 0.6.1"},
-     {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.6.0"}]
+    [{:nerves, "~> 0.4.0"},
+     {:nerves_system_br, "~> 0.9.2"},
+     {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.9.0"}]
   end
 
   defp description do
--- nerves.exs
+++ nerves.exs
@@ -8,17 +8,32 @@ version =
 config :nerves_system_nanopi_neo, :nerves_env,
   type: :system,
   version: version,
-  mirrors: ["http://www.nine70labs.com/nerves/nerves_system_nanopi_neo_0_0_11.tar.gz"],
-  build_platform: Nerves.System.Platforms.BR,
-  build_config: [
-    defconfig: "nerves_defconfig",
-    package_files: [
-      "busybox_defconfig",
-      "rootfs-additions",
-      "fwup.conf",
-      "post-createfs.sh",
-      "uboot",
-      "uboot-script.cmd",
-      "linux"
-    ]
+  # mirrors: ["http://www.nine70labs.com/nerves/nerves_system_nanopi_neo_0_0_11.tar.gz"],
+  compiler: :nerves_package,
+  artifact_url: ["http://www.nine70labs.com/nerves/nerves_system_nanopi_neo_0_0_11.tar.gz"],
+  platform: Nerves.System.BR,
+  platform_config: [
+    defconfig: "nerves_defconfig"
+  ],
+  checksum: [
+    "busybox_defconfig",
+    "rootfs-additions",
+    "fwup.conf",
+    "post-createfs.sh",
+    "uboot",
+    "uboot-script.cmd",
+    "linux"
   ]
+  # build_platform: Nerves.System.Platforms.BR,
+  # build_config: [
+  #   defconfig: "nerves_defconfig",
+  #   package_files: [
+  #     "busybox_defconfig",
+  #     "rootfs-additions",
+  #     "fwup.conf",
+  #     "post-createfs.sh",
+  #     "uboot",
+  #     "uboot-script.cmd",
+  #     "linux"
+  #   ]
+  # ]
