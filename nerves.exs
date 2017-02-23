4ee8d4ed28e658cf8ec95adb3c5764aacc80d8d4
Troy Nichols <troygnichols@gmail.com>
2017-02-23 13:33:08 -0500

    Use github artifact URL

--- nerves.exs
+++ nerves.exs
@@ -10,7 +10,7 @@ config :nerves_system_nanopi_neo, :nerves_env,
   version: version,
   # mirrors: ["http://www.nine70labs.com/nerves/nerves_system_nanopi_neo_0_0_11.tar.gz"],
   compiler: :nerves_package,
-  artifact_url: ["http://www.nine70labs.com/nerves/nerves_system_nanopi_neo_0_0_11.tar.gz"],
+  artifact_url: ["https://github.com/troygnichols/nerves_system_nanopi_neo/releases/download/v0.11/nerves_system_nanopi_neo_0_0_11.tar.gz"],
   platform: Nerves.System.BR,
   platform_config: [
     defconfig: "nerves_defconfig"
@@ -24,16 +24,3 @@ config :nerves_system_nanopi_neo, :nerves_env,
     "uboot-script.cmd",
     "linux"
   ]
-  # build_platform: Nerves.System.Platforms.BR,
-  # build_config: [
-  #   defconfig: "nerves_defconfig",
-  #   package_files: [
-  #     "busybox_defconfig",
-  #     "rootfs-additions",
-  #     "fwup.conf",
-  #     "post-createfs.sh",
-  #     "uboot",
-  #     "uboot-script.cmd",
-  #     "linux"
-  #   ]
-  # ]
